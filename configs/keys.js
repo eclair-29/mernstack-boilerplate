4133691d376f193fc82e60a72239bc9eb13a8f78
Migz De Chavez <dmgzky@gmail.com>
2019-03-11 15:03:01 +0800

    update connection string

--- configs/keys.js
+++ configs/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   // You might remove this URL and replace it with your own connection string
-  mongodbURI: "mongodb+srv://eclair-29:eclair-29@mernstack-boilerplate-test-db-quo2j.mongodb.net/test?retryWrites=true",
+  mongodbURI: "mongodb+srv://eclair-29:eclair-29@mernstack-boilerplate-info-db-quo2j.mongodb.net/test?retryWrites=true",
   secretOrKey: "SECRET_OR_KEY"
 };
