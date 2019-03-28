f31cd4da19f701fccb082d6cad5fdcec7ff773ab
Migz De Chavez <dmgzky@gmail.com>
2019-03-28 18:33:34 +0800

    update some minor config

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
+/* eslint-disable no-console */
 import React from "react";
 import axios from "axios";
 
-import brand from "./brand.svg";
-
 class App extends React.Component {
   state = {
-    data: undefined
+    data: null
   };
 
   componentDidMount() {
@@ -16,18 +15,41 @@ class App extends React.Component {
 
   render() {
     const { data } = this.state;
-
-    const content = data ? (
-      <div className="wrapper">
-        <img src={brand} alt="mernstack-brand" />
-
-        <h1 className="headline">
-          a mern stack boilerplate created by
-          {` ${data.fname} ${data.lname} `}
-          [eclair]
-        </h1>
+    const content = (
+      <div className="wrapper headline">
+        a mern stack development boilerplate, see console for app details
       </div>
-    ) : null;
+    );
+
+    if (data) {
+      console.log(
+        "%c Welcome to your fresh Mern Stack Starter Boilerplate",
+        "color: #2196F3; font-weight: 600;"
+      );
+
+      console.log(
+        "%c ****************************************************",
+        "color: #2196F3; font-weight: 600;"
+      );
+
+      console.log(
+        "app name: %c mernstack-boilerplate",
+        "color: #2196F3; font-weight: 600;"
+      );
+
+      console.log(
+        `author: %c ${data.fname} ${data.lname}`,
+        "color: #2196F3; font-weight: 600; text-transform: capitalize;"
+      );
+
+      console.log(
+        `version: %c ${data.version}`,
+        "color: #2196F3; font-weight: 600;"
+      );
+
+      console.log(`github repo: ${data.github_repo_URL}`);
+      console.log(`github page: ${data.github_page_URL}`);
+    }
 
     return <div className="App">{content}</div>;
   }
